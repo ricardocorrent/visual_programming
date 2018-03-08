57a9a891560b91c48def384b8d16cae2413d50ce
Ricardo Corrent <ricardocorrent@hotmail.com>
2018-03-08 10:33:16 -0300

    simple calculator

---
+++ Calculator/Calculator.pas
@@ -0,0 +1,43 @@
+unit Calculator;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    lblDisplay: TLabel;
+    lblNumbers: TLabel;
+    btn0: TButton;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    btn6: TButton;
+    btn7: TButton;
+    btn8: TButton;
+    btn9: TButton;
+    btnDot: TButton;
+    btnDivide: TButton;
+    btnMultiply: TButton;
+    btnSubtract: TButton;
+    btnAdd: TButton;
+    btnResult: TButton;
+    btnClean: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ Calculator/ProjectCalculator.dpr
@@ -0,0 +1,14 @@
+program ProjectCalculator;
+
+uses
+  Vcl.Forms,
+  Calculator in 'Calculator.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
